20111e2a65c22ebd5e94a5d96631610a1d201421
Joel <joel.b.williams.is@gmail.com>
2018-01-16 10:04:36 -0600

    post to create playpen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Playpen from './Playpen';
+// import Playpen from './Playpen';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +10,33 @@ class App extends Component {
     toDo: 'Time to create a Playpen instance & display it'
   }
 
-//   componentDidMount(){
-// // call to db to get all of this playpen's blocks
-// // editor's note - need to have an instance of playpen first
-//   }
+  // this.newPlaypen = this.newPlaypen.bind(this);
+
+  newPlaypen(e) {
+    e.preventDefault();
+    console.log("add a playpen");
+    // need to POST a new playpen & make it the current state
+    let playpenData = { name: this.refs.playpen_name.value };
+
+    var request = new Request('http://localhost:3001/api/new-playpen', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(playpenData)
+    });
+
+    fetch(request)
+    .then(function(response) {
+      // console.log({response});
+      response.json().then(function(data) {
+        // console.log({data});
+      });
+    })
+    .catch(function(err) {
+      console.log('caught in app.js')
+      console.log(err);
+    });
+
+  }
 
         // <Playpen></Playpen>
   render() {
@@ -26,6 +49,10 @@ class App extends Component {
         <p className="App-intro">
           {this.state.toDo}
         </p>
+        <form ref="playpenForm">
+          <input type="text" ref="playpen_name" placeholder="Playpen Name" />
+          <button onClick={this.newPlaypen.bind(this)}> New Playpen </button>
+        </form>
       </div>
     );
   }
